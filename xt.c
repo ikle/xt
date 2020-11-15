c9c72fedf9189e6671974b79132279c0460a5db6
Alexei A. Smekalkine <ikle@ikle.ru>
2020-11-15 15:20:50 +0300

    Add core prototype

---
+++ xt.c
@@ -0,0 +1,242 @@
+/*
+ * X Tables Control
+ *
+ * Copyright (c) 2020 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <data/hash.h>
+#include <data/ht.h>
+#include <data/seq.h>
+
+#include "xt.h"
+
+/* xt rule */
+
+struct xt_rule {
+	struct xt_rule *next;
+};
+
+struct xt_rule *xt_rule_alloc (const char *domain)
+{
+	struct xt_rule *o;
+
+	if ((o = malloc (sizeof (*o))) == NULL)
+		return NULL;
+
+	o->next = NULL;
+	return o;
+}
+
+void xt_rule_free (struct xt_rule *o)
+{
+	free (o);
+}
+
+/* xt chain */
+
+#define XT_NAME_LEN  32
+
+static int xt_name_init (char *name, const char *value)
+{
+	if (snprintf (name, XT_NAME_LEN, "%s", name) < XT_NAME_LEN)
+		return 1;
+
+	errno = EINVAL;
+	return 0;
+}
+
+SEQ_DECLARE (xt_rule);
+
+struct xt_chain {
+	char name[XT_NAME_LEN];
+	char policy[XT_NAME_LEN];
+	struct xt_rule_seq rules;
+};
+
+struct xt_chain *xt_chain_alloc (const char *name, const char *policy)
+{
+	struct xt_chain *o;
+
+	if ((o = malloc (sizeof (*o))) == NULL)
+		return NULL;
+
+	if (!xt_name_init (o->name, name))
+		goto no_name;
+
+	if (!xt_name_init (o->policy, policy == NULL ? "" : policy))
+		goto no_policy;
+
+	xt_rule_seq_init (&o->rules);
+	return o;
+no_policy:
+no_name:
+	free (o);
+	return 0;
+}
+
+static void xt_chain_free (void *O)
+{
+	struct xt_chain *o = O;
+
+	if (o == NULL)
+		return;
+
+	xt_rule_seq_fini (&o->rules, xt_rule_free);
+	free (o);
+}
+
+static int xt_chain_eq (const void *O, const void *K)
+{
+	const struct xt_chain *o = O;
+	const struct xt_chain *k = K;
+
+	return strcmp (o->name, k->name) == 0;
+}
+
+static size_t xt_chain_hash (const void *O)
+{
+	const struct xt_chain *o = O;
+
+	return hash (0, o->name, strlen (o->name));
+}
+
+static const struct data_type xt_chain_type = {
+	.free	= xt_chain_free,
+	.eq	= xt_chain_eq,
+	.hash	= xt_chain_hash,
+};
+
+/* xt core */
+
+struct xt {
+	struct ht ht;
+};
+
+struct xt *xt_alloc (const char *domain)
+{
+	struct xt *o;
+
+	if ((o = malloc (sizeof (*o))) == NULL)
+		return NULL;
+
+	if (!ht_init (&o->ht, &xt_chain_type))
+		goto no_ht;
+
+	return o;
+no_ht:
+	free (o);
+	return NULL;
+}
+
+void xt_free (struct xt *o)
+{
+	if (o == NULL)
+		return;
+
+	ht_fini (&o->ht);
+	free (o);
+}
+
+const char *xt_error (struct xt *o);
+
+int xt_is_chain (struct xt *o, const char *chain)
+{
+	struct xt_chain key;
+
+	if (!xt_name_init (key.name, chain))
+		return 0;
+
+	return ht_lookup (&o->ht, &key) != NULL;
+}
+
+int xt_create_chain (struct xt *o, const char *chain, const char *policy)
+{
+	struct xt_chain *c;
+	
+	if ((c = xt_chain_alloc (chain, policy)) == NULL)
+		return 0;
+
+	if (ht_insert (&o->ht, c, 0))
+		return 1;
+
+	xt_chain_free (c);
+	return 0;
+}
+
+int xt_delete_chain (struct xt *o, const char *chain)
+{
+	struct xt_chain key;
+
+	if (!xt_name_init (key.name, chain))
+		return 0;
+
+	ht_remove (&o->ht, &key);
+	return 1;
+}
+
+int xt_set_policy (struct xt *o, const char *chain, const char *policy)
+{
+	struct xt_chain *c, key;
+
+	if (!xt_name_init (key.name, chain))
+		return 0;
+
+	if ((c = ht_lookup (&o->ht, &key)) == NULL) {
+		errno = ENOENT;
+		return 0;
+	}
+
+	return xt_name_init (c->policy, policy);
+}
+
+int xt_flush_chain (struct xt *o, const char *chain)
+{
+	struct xt_chain *c, key;
+
+	if (!xt_name_init (key.name, chain))
+		return 0;
+
+	if ((c = ht_lookup (&o->ht, &key)) == NULL) {
+		errno = ENOENT;
+		return 0;
+	}
+
+	xt_rule_seq_fini (&c->rules, xt_rule_free);
+	xt_rule_seq_init (&c->rules);
+	return 1;
+}
+
+int xt_append_rule (struct xt *o, const char *chain, struct xt_rule *r)
+{
+	struct xt_chain *c, key;
+
+	if (!xt_name_init (key.name, chain))
+		return 0;
+
+	if ((c = ht_lookup (&o->ht, &key)) == NULL) {
+		errno = ENOENT;
+		return 0;
+	}
+
+	xt_rule_seq_enqueue (&c->rules, r);
+	return 1;
+}
+
+int xt_iterate (struct xt *o, xt_cb *cb, void *cookie)
+{
+	size_t i;
+	struct xt_chain *c;
+
+	ht_foreach (i, c, &o->ht)
+		if (!cb (o, c->name, cookie))
+			return 0;
+
+	return 1;
+}
---
+++ xt.h
@@ -0,0 +1,42 @@
+/*
+ * X Tables Control
+ *
+ * Copyright (c) 2020 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef NET_XT_H
+#define NET_XT_H  1
+
+#include <stddef.h>
+
+/* xt rule */
+
+struct xt_rule *xt_rule_alloc (const char *domain);
+void xt_rule_free (struct xt_rule *o);
+
+/* xt core */
+
+struct xt *xt_alloc (const char *domain);
+void xt_free (struct xt *o);
+
+const char *xt_error (struct xt *o);
+
+int xt_is_chain (struct xt *o, const char *chain);
+
+int xt_create_chain (struct xt *o, const char *chain, const char *policy);
+int xt_delete_chain (struct xt *o, const char *chain);
+
+int xt_set_policy   (struct xt *o, const char *chain, const char *policy);
+int xt_flush_chain  (struct xt *o, const char *chain);
+
+int xt_append_rule  (struct xt *o, const char *chain, struct xt_rule *r);
+
+/* xt info */
+
+typedef int (xt_cb) (struct xt *o, const char *chain, void *cookie);
+
+int xt_iterate (struct xt *o, xt_cb *cb, void *cookie);
+
+#endif  /* NET_XT_H */
